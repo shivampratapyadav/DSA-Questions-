4a33922db6a8ab5e54ece7f15c1a005daaef6f7a
Shivam <shivampratapyadav@gmail.com>
2021-08-09 03:37:19 +0530

    Initial commit

---
+++ LEVEL 1/01 Basics Of Programming/a. Getting Started/cwa01PrintZ.java
@@ -0,0 +1,28 @@
+/*09-08-2021
+MONDAY
+
+QUESTION : 1. You are required to print a 'z' of size 5 using '*'.
+
+
+Input Format:
+There is no input.
+
+
+Output Format:
+*****
+   *
+  *
+ *
+*****
+
+*/
+
+public class cwa01PrintZ 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("*****\n   *\n  *\n *\n*****");
+    }
+    
+}
