3be1ae138761ef004d0de0a97004c4ed4996b9be
Shivam <shivampratapyadav@gmail.com>
2021-11-16 23:58:30 +0530

    Linked List half Completed

--- LEVEL 1/04 Basic Data Structures/Stacks and Queues/cwa13SmallestNumberFollowingPattern.java
+++ LEVEL 1/04 Basic Data Structures/Stacks and Queues/cwa13SmallestNumberFollowingPattern.java
@@ -1,1 +1,78 @@
-cwa13SmallestNumberFollowingPattern
+/*
+14-11-2021
+SATURDAY
+
+QUESTION -> Smallest Number Following Pattern :
+
+1. You are given a pattern of upto 8 length containing characters 'i' and 'd'.
+2. 'd' stands for decreasing and 'i' stands for increasing
+3. You have to print the smallest number, using the digits 1 to 9 only without repetition, such that 
+the digit decreases following a d and increases follwing an i.
+
+e.g.
+d -> 21
+i -> 12
+ddd -> 4321
+iii -> 1234
+dddiddd -> 43218765
+iiddd -> 126543
+
+Input Format:
+Input is managed for you.
+
+Output Format:
+Smallest sequence of digits (from 1 to 9) without duplicacy and following the pattern.
+
+Constraints:
+0 < str.length <= 8
+str contains only 'd' and 'i'
+
+Sample Input:
+ddddiiii
+
+Sample Output:
+543216789
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class cwa13SmallestNumberFollowingPattern{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+
+    // code
+
+    Stack<Integer> st = new Stack<>();
+    
+    int i=0;
+    
+    for(i=0;i<str.length();i++){
+        char ch = str.charAt(i);
+
+        if(ch == 'd'){
+            st.push(i+1);
+        }
+        else if(ch=='i'){
+            st.push(i+1);
+
+            while(st.size()>0){
+                System.out.print(st.pop());
+            }
+
+
+        }
+    }
+
+ 
+    st.push(i+1);
+    while(st.size()>0){
+        System.out.print(st.pop());
+    }
+    
+ }
+}
